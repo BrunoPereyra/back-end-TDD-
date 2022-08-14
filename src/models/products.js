199981bfeff29d1c27caa0c375e0cfae1e915081
Bruno Pereyra <brunopereyra459@gmail.com>
2022-08-14 14:44:14 -0300

    products v1 model

--- src/models/products.js
+++ src/models/products.js
@@ -5,5 +5,25 @@ const SchemaProducts = new Schema({
       type: Schema.Types.ObjectId,
       ref: "Users"
    }],
+   type: String,
    characteristic: Array,
+   productFeedback: [{
+      type: Schema.Types.ObjectId,
+      ref: "productFeedback"
+   }],
+   stock:Number,
+   productStars:{
+      cantidad:Number,
+      promedio:Number,
+      starts:{
+         one:Number,
+         two:Number,
+         three:Number,
+         four:Number,
+         five:Number
+      }
+   },
+   
 })
+const Products = model(Products,SchemaProducts)
+module.exports = Products
