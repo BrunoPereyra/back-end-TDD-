5c13bb3608963ad18875039e1a3be5b89648c2b1
Bruno Pereyra <brunopereyra459@gmail.com>
2022-08-28 12:49:57 -0300

    missing product

--- src/controllers/GETproducts.js
+++ src/controllers/GETproducts.js
@@ -2,10 +2,78 @@ const Products = require("../models/products")
 
 const GETproducts = async (req, ress) => {
     const { nameProduct, id } = req.query
-    console.log(nameProduct)
-    const product = await Products.find({nameProduct:"este es el nombre"})
-    return ress.status(200).json({
-        ress: product
-    })
+    var product = ""
+    if (id) {
+        if (id.length == 24 && typeof id == "string") {
+            try {
+
+                product = await Products.findById(id)
+                    .sort({ date: -1 })
+                    .populate({
+                        path: "user",
+                        select: {},
+                        match: {},
+                        options: { sort: { date: -1 } },
+                    })
+                if (product == null) {
+                    return ress.status(404).json({
+                        ress: product
+                    })
+                } else {
+                    return ress.status(200).json({
+                        ress: product
+                    })
+                }
+            } catch (error) {
+                return ress.status(404).json({
+                    ress: null
+                })
+            }
+        } else {
+            return ress.status(404).json({
+                ress: "missing data or malformed id"
+            })
+        }
+    }
+   console.log(nameProduct)
+    product = await Products.find({ nameProduct })
+        .sort({ date: -1 })
+        .populate({
+            path: "user",
+            select: {},
+            match: {},
+            options: { sort: { date: -1 } },
+        })
+    console.log(product)
+    // if (typeof (nameProduct) == "string") {
+    //     try {
+    //         product = await Products.find({ nameProduct: nameProduct })
+    //             .sort({ date: -1 })
+    //             .populate({
+    //                 path: "user",
+    //                 select: {},
+    //                 match: {},
+    //                 options: { sort: { date: -1 } },
+    //             })
+
+    //         if (product.length > 0) {
+    //             return ress.status(200).json({
+    //                 ress: product
+    //             })
+    //         } else {
+    //             return ress.status(404).json({
+    //                 ress: product
+    //             })
+    //         }
+    //     } catch (error) {
+    //         return ress.status(404).json({
+    //             ress: product
+    //         })
+    //     }
+    // } else {
+    //     return ress.status(200).json({
+    //         ress: "missing data or malformed nameProduct"
+    //     })
+    // }
 }
 module.exports = GETproducts
--- src/tests/searchProducts.test.js
+++ src/tests/searchProducts.test.js
@@ -5,11 +5,33 @@ const { GETproducts } = require("./helpers")
 
 describe("GET - /products:URL", () => {
     test("GET - nameproducts ok", async () => {
-        const ress = await GETproducts("este es el nombre", "abbasds")
-        const product = await Products.find({nameProduct:"este es el nombre"})
-
-        expect(ress.body.ress).toEqual(product)
+        const ress = await GETproducts("este es el nombre")
+        expect(JSON.stringify(ress._body.ress).length >= 1).toBeTruthy()
         expect(ress.statusCode).toBe(200)
+    })
+    test("GET - id a coincidence", async () => {
+        const ress = await GETproducts("este es el nombre", "62fad15c4a49306413d261bd")
+
+        expect(ress._body.ress._id == "62fad15c4a49306413d261bd").toBeTruthy()
+        expect(ress.statusCode).toBe(200)
+    })
+    test("GET - there are no coincidences nameProduct", async () => {
+        const ress = await GETproducts("dsds")
+
+        expect(JSON.stringify(ress._body.ress).length == 0).toBeTruthy()
+        expect(ress.statusCode).toBe(404)
+    })
+    test("GET - there are no coincidences id", async () => {
+        const ress = await GETproducts("dsds","62fad15c4a49306413d261b9")
+
+        expect(ress._body.ress == null).toBeTruthy()
+        expect(ress.statusCode).toBe(404)
+    })
+    test("GET - id error or missing ", async () => {
+        const ress = await GETproducts("oihjoih", "62fadhi61bda")
+
+        expect(ress._body.ress).toBe("missing data or malformed id")
+        expect(ress.statusCode).toBe(404)
     })
 })
 afterAll(() => {
